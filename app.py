32aa8099955428b28bd2ec8b26898c6b978040de
kanAfridi <kafridi281@gmail.com>
2024-12-18 01:15:59 +0500

    deploy the model

--- app.py
+++ app.py
@@ -1,25 +1,85 @@
 import streamlit as st
 import sklearn
 import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Load the model
+# Load from pickle Model file
 try:
     with open("Model.pkl", "rb") as f:
         model_data = pickle.load(f)
         print("Model loaded successfully.")
 except FileNotFoundError:
     print("Model file not found. Please make sure to train the model first.")
-    exit()  
-    
+
+# Load from pickle Victorizer file
+
+try:
+    with open("Tfidf.pkl", "rb") as f:
+        tfidf = pickle.load(f)
+        print("Victorizer loaded successfully.")
+except FileNotFoundError:
+    print("Victorizer file not found.")
+ 
+# Load the model data
 model = model_data["model"]
 threshold = model_data["threshold"]
 
 
+# Title for the webapp
 st.title("Message Spam Detector")
 
-message = st.text_area("Message Spam Detector")
+# Input text preprocessing function
+def preprocess_text(text):
+    swords = stopwords.words('english')
+    stemmer = SnowballStemmer('english')
+    
+    data = text.lower()
+    
+    tokenzie = nltk.word_tokenize(data)
+    
+    filter_tokenzie = []
+    for word in tokenzie:
+        if word.isalnum() and len(word) > 1:
+            filter_tokenzie.append(word)
+    
+    stopwords_filter = []
+    
+    for word in filter_tokenzie:
+        if word not in swords:
+            stopwords_filter.append(word)
+    
+    stemming_filter = []
+    for word in stopwords_filter:
+        stemming_filter.append(stemmer.stem(word))
+        
+    return " ".join(stemming_filter)
 
 
-# 2. Make predictions with the saved threshold
-new_probs = model.predict_proba(message)[:, 1]
-new_pred = (new_probs >= threshold).astype(int)
+
+# 1. Input text
+text_input = st.text_area("Message Spam Detector")
+
+# 2. Preprocess the input text
+transformed_text = preprocess_text(text_input)
+
+# 3. Text into vector
+#tfidf = TfidfVectorizer(max_features = 3000)
+vector = tfidf.transform([transformed_text])
+
+# 3. Make predictions with the saved threshold
+proba = model.predict_proba(vector)[:, 1]
+prediction = (proba >= threshold).astype(int)
+
+# 4. Display the predictions
+if st.button('Predict'):
+    if prediction[0] == 0:
+        st.success("This message is not spam.")
+    elif prediction[0] == 1:
+        st.warning("This message is spam.")
+        st.write("Probability of being spam:", proba[0])
+    else:
+        st.error("An error occurred while processing the message.")   
+
